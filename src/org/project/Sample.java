c0c104e3dfe11b113cb161b37e9506579b367c64
Krishnamoorthy-Murugan <mkrishna1998tvm@gmail.com>
2022-07-03 14:25:42 +0530

    gopi done

--- src/org/project/Sample.java
+++ src/org/project/Sample.java
@@ -1,13 +1,14 @@
 package org.project;
 
 public class Sample {
-<<<<<<< HEAD
 	public void Bala() {
 System.out.println("Bala method");
-=======
+	}
 	public void krish() {
 System.out.println("Krish method");
->>>>>>> ef017c6bdc813af68a6fd03a3b22eb3f75d43682
+	}
+	private void gopi() {
+System.out.println("gopi method");
 	}
 public static void main(String[] args) {
 	System.out.println("work done");
