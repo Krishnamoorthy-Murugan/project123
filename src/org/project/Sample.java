97599658c83d224f97241623967ded18f36cb6da
Krishnamoorthy-Murugan <mkrishna1998tvm@gmail.com>
2022-07-03 13:55:23 +0530

    Bala done the work

--- src/org/project/Sample.java
+++ src/org/project/Sample.java
@@ -1,6 +1,9 @@
 package org.project;
 
 public class Sample {
+	public void Bala() {
+System.out.println("Bala method");
+	}
 public static void main(String[] args) {
 	System.out.println("work done");
 }
